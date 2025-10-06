51fce9c18eacebef3699a81bd879fd6c5722aa5a
Tushar Agrahari <tushar84_@Tushars-MacBook-Air.local>
2025-10-07 01:50:23 +0530

    server started

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use serde::{Deserialize, Serialize};
+
+
+#[get("/")]
+async fn home() -> impl Responder {
+    HttpResponse::Ok().body("Welcome to Actix Web Server 🚀")
+}
+
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    println!("Server running on http://localhost:8080");
+
+    HttpServer::new(|| {
+        App::new()
+            .service(home)
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
